32aefb5a59f3aa95749efffb2fe32f5fc65634b2
Croma-jet <86805577+Croma-jet@users.noreply.github.com>
2023-02-06 18:26:04 +0100

    3 files modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import "./style.css";
 
 export default function App() {
   return (
+    <div class="nav">
+<a href="nav_about">About</a>
+      </div>
+  );
+}
+
+export default function App() {
+  return (
     <div>
       <h1>CromajetEx</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <p>Barka dazowa wanan shafi na cromajetEx dan koyan ginin yanar gizo da harshen hausa:)</p>
     </div>
+
   );
 }
